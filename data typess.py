b15a95262f27e1e5ad71031c2467a65cba375cad
navyashreegovindswamy <navyashreegovindswamy@gmail.com>
2026-05-06 10:26:29 +0530

    Add files via upload

---
+++ data typess.py
@@ -0,0 +1,8 @@
+x1=10
+x2=10.5
+x3=10+3j
+x4="hello"
+print("the data type of x(1)is " ,type(x1))
+print("the data type of (x2) is ",type(x2))
+print("the data type of (x3) is " ,type(x3))
+print("the data type of (x4) is " ,type(x4))
---
+++ pattern formation.py
@@ -0,0 +1,18 @@
+name = "NAVYASHREE"
+
+for i in range(len(name)):
+    for j in range(i + 1):
+        print(name[j], end="")
+    print()
+
+n=5;
+for i in range(n):
+    for j in range(i):
+        print("*", end="")
+    print()
+
+for i in range(n,0,-1):
+    for j in range(i):
+        print("*", end="")
+    print()
+
